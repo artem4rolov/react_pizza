9f56db6ea1639409de44a6a254547d39942d298c
Artem <artem4rolov@gmail.com>
2023-04-01 10:58:50 +0300

    список пицц

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import Header from "./components/Header/Header";
 import Categories from "./components/Categories/Categories";
 import Sort from "./components/Sort/Sort";
+import PizzaBlock from "./components/PizzaBlock/PizzaBlock";
+import pizzas from "./assets/db.json";
 
 import "./scss/app.scss";
-import PizzaBlock from "./components/PizzaBlock/PizzaBlock";
 
 function App() {
   return (
-    <div class="wrapper">
+    <div className="wrapper">
       <Header />
-      <div class="content">
-        <div class="container">
-          <div class="content__top">
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
             <Categories />
             <Sort />
           </div>
-          <h2 class="content__title">Все пиццы</h2>
-          <div class="content__items">
-            <PizzaBlock title="Мексиканская" price="500" />
-            <PizzaBlock />
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            {pizzas.map((pizza, index) => (
+              <PizzaBlock key={pizza.title} {...pizza} />
+            ))}
           </div>
         </div>
       </div>
--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,13 +1,39 @@
+import React from "react";
+
 function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(3);
+
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
+
   return (
-    <div class="categories">
+    <div className="categories">
       <ul>
-        <li class="active">Все</li>
+        {categories.map((category, index) => (
+          <li
+            key={index}
+            onClick={() => onClickCategory(index)}
+            className={activeIndex === index ? "active" : ""}
+          >
+            {category}
+          </li>
+        ))}
+        {/* <li className="active">Все</li>
         <li>Мясные</li>
         <li>Вегетарианская</li>
         <li>Гриль</li>
         <li>Острые</li>
-        <li>Закрытые</li>
+        <li>Закрытые</li> */}
       </ul>
     </div>
   );
